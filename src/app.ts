4edb37bf0abe126a036f5a78178c8007fbb58a6d
rudikurniawan99 <rudikurniawan501@gmail.com>
2021-12-28 14:48:21 +0700

    create tsconfig and setup config

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,10 @@
-console.log('coba ini broo')
+import express from 'express'
+import config from 'config'
+
+const port = config.get<number>('port')
+
+const app = express()
+
+app.listen(port, () => {
+  console.log(`App is running at port:${port}`)
+})
