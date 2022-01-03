b21515781d8eda53dd534d097cc0706a5affe582
rudikurniawan99 <rudikurniawan501@gmail.com>
2022-01-03 17:10:00 +0700

    make user.model

---
+++ models/user.model.ts
@@ -0,0 +1,55 @@
+import { NextFunction } from 'express'
+import mongoose from 'mongoose'
+import bcrypt from 'bcrypt'
+import { string } from 'zod'
+
+export interface UserDocument extends mongoose.Document {
+  email: string
+  name: string
+  password: string
+  createdAt: Date
+  updatedAt: Date
+  comparePassword: (pass: string) => Promise<boolean>
+}
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+},{
+  timestamps: true
+})
+
+userSchema.pre("save", async function(next) {
+  let user = this as UserDocument
+
+  if(!user.isModified('password')){
+    return next()
+  }
+
+  const salt = await bcrypt.genSalt(10)
+  const hash = await bcrypt.hashSync(user.password, salt)
+  user.password = hash
+  return next()
+})
+
+userSchema.methods.comparePassword = async function (pass: string): Promise<boolean>{
+  const user = this as UserDocument
+
+  return await bcrypt.compare(pass, user.password)
+    .catch((e) => false)
+}
+
+const User = mongoose.model('User', userSchema)
+
+export default User
